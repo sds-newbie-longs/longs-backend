174a16d1d07384cba268eec47c364a996c6a0ef3
seonpilKim <ksp970306@gmail.com>
2023-06-07 21:13:04 +0900

    [SL-2] chore: 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/sds/actlongs/ActLongsApplication.java
@@ -0,0 +1,13 @@
+package com.sds.actlongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActLongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActLongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sds/actlongs/ActLongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.actlongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActLongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
