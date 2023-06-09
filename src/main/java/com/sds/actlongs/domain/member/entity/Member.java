fa60d9cec6f68ad4e7a8bb33ec2150e393b69905
nureesong <nuri3136@naver.com>
2023-06-09 18:41:51 +0900

    [SL-100] feat: Member Entity 추가

---
+++ src/main/java/com/sds/actlongs/domain/member/entity/Member.java
@@ -0,0 +1,43 @@
+package com.sds.actlongs.domain.member.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sds.actlongs.domain.BaseEntity;
+import com.sds.actlongs.vo.ImageExtension;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor
+public class Member extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 20)
+	private String username;
+
+	@Column(unique = true, length = 36)
+	private String profileImageUuid;
+
+	@Enumerated(EnumType.STRING)
+	private ImageExtension profileImageType;
+
+	public Member(String username) {
+		this.username = username;
+		this.profileImageUuid = null;
+		this.profileImageType = null;
+	}
+
+}
---
+++ src/main/java/com/sds/actlongs/domain/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.sds.actlongs.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sds.actlongs.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
