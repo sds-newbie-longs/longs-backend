fb7eb7bdf7d53bdc2b57f72e12ff51501f3436dc
hongsam123 <a2670624@naver.com>
2023-06-12 11:25:32 +0900

    [SL-120] S3Uploead 클래스 동영상 업로드 추가

---
+++ src/main/java/com/sds/actlongs/util/FileNameConstants.java
@@ -0,0 +1,7 @@
+package com.sds.actlongs.util;
+
+public final class FileNameConstants {
+	public static final String CATEGORY_PREFIX = "/";
+	public static final String TIME_SEPARATOR = "_";
+	public static final String FILE_EXTENSION_SEPARATOR = ".";
+}
