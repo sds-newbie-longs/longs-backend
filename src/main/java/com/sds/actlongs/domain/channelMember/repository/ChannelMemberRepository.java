9d81bef2402d9eb4312db3ab3268c138fea0a89e
nureesong <nuri3136@naver.com>
2023-06-13 11:11:54 +0900

    [SL-102] style: formatter 적용

--- src/main/java/com/sds/actlongs/domain/channelMember/repository/ChannelMemberRepository.java
+++ src/main/java/com/sds/actlongs/domain/channelMember/repository/ChannelMemberRepository.java
@@ -1,2 +1,9 @@
-package com.sds.actlongs.domain.channelMember.repository;public interface ChannelMemberRepository {
+package com.sds.actlongs.domain.channelMember.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sds.actlongs.domain.channelMember.entity.ChannelMember;
+
+public interface ChannelMemberRepository extends JpaRepository<ChannelMember, Long> {
+
 }
