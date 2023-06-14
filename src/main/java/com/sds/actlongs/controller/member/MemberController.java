7c75c975677a2f422e774852db098f723af2f4b7
Sean_R <ksp970306@gmail.com>
2023-06-15 00:39:44 +0900

    [NO-ISSUE] docs: 로그인 API 문서 업데이트
    - API 결과 비즈니스 코드 정보 추가
    - 요청 모델(username) 검증 정보 추가

--- src/main/java/com/sds/actlongs/controller/member/MemberController.java
+++ src/main/java/com/sds/actlongs/controller/member/MemberController.java
@@ -25,7 +25,9 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@ApiOperation(value = "로그인 API")
+	@ApiOperation(value = "로그인 API", notes = ""
+		+ "M001: 로그인에 성공하였습니다.\n"
+		+ "M002: 로그인에 실패하였습니다.")
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponse> login(@Valid @RequestBody final LoginRequest request,
 		final HttpServletRequest servletRequest) {
