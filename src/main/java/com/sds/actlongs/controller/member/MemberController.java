e2373133022ea689fbabc7b2dbcd6a8556b80bf1
Sean_R <ksp970306@gmail.com>
2023-06-15 00:31:08 +0900

    [NO-ISSUE] fix: @Valid 추가 및 @Validated 제거

--- src/main/java/com/sds/actlongs/controller/member/MemberController.java
+++ src/main/java/com/sds/actlongs/controller/member/MemberController.java
@@ -1,9 +1,9 @@
 package com.sds.actlongs.controller.member;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +18,6 @@ import com.sds.actlongs.controller.member.dto.LoginResponse;
 import com.sds.actlongs.service.member.MemberService;
 
 @Api(tags = "회원 API")
-@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -28,8 +27,9 @@ public class MemberController {
 
 	@ApiOperation(value = "로그인 API")
 	@PostMapping("/login")
-	public ResponseEntity<LoginResponse> login(@RequestBody final LoginRequest dto, final HttpServletRequest request) {
-		final boolean result = memberService.login(dto.getUsername(), request.getSession());
+	public ResponseEntity<LoginResponse> login(@Valid @RequestBody final LoginRequest request,
+		final HttpServletRequest servletRequest) {
+		final boolean result = memberService.login(request.getUsername(), servletRequest.getSession());
 		return ResponseEntity.ok(LoginResponse.of(result));
 	}
 
