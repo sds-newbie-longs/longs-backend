546a41f006aef84d9f7853b2b370eb8a979741f8
ChangJinMoon <jin1004boy@naver.com>
2023-06-15 17:52:59 +0900

    [SL-25] fix: 충돌해결 완료

--- src/test/java/com/sds/actlongs/ActLongsApplicationTests.java
+++ src/test/java/com/sds/actlongs/ActLongsApplicationTests.java
@@ -3,9 +3,8 @@ package com.sds.actlongs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ActLongsApplicationTests {
-	@Test
+
 	void contextLoads() {
 
 	}
