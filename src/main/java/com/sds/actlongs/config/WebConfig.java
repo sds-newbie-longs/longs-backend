b7efcac8ab9fffdf93b14a38706a4a8adf385034
Sean_R <ksp970306@gmail.com>
2023-06-16 15:32:18 +0900

    [NO-ISSUE] refactor: 상수 변경

--- src/main/java/com/sds/actlongs/config/WebConfig.java
+++ src/main/java/com/sds/actlongs/config/WebConfig.java
@@ -5,6 +5,8 @@ import static com.sds.actlongs.util.Constants.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,9 +21,9 @@ public class WebConfig {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry
 					.addMapping(ALL_PATH)
-					.allowedMethods(WILDCARD)
+					.allowedMethods(CorsConfiguration.ALL)
 					.allowedOrigins("http://localhost:3000")
-					.allowedHeaders(WILDCARD)
+					.allowedHeaders(CorsConfiguration.ALL)
 					.allowCredentials(true);
 			}
 		};
