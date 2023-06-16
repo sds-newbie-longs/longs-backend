1ee12ea2f116265a9e5ad032e46909478aef22be
hongsam123 <a2670624@naver.com>
2023-06-16 21:13:19 +0900

    [SL-159] docs: 게시글 수정 API Swagger 작성

--- src/main/java/com/sds/actlongs/controller/board/BoardController.java
+++ src/main/java/com/sds/actlongs/controller/board/BoardController.java
@@ -1,12 +1,10 @@
 package com.sds.actlongs.controller.board;
 
-import java.sql.Time;
-import java.time.LocalTime;
-
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +15,8 @@ import lombok.RequiredArgsConstructor;
 
 import com.sds.actlongs.controller.board.dto.BoardDetailResponse;
 import com.sds.actlongs.controller.board.dto.BoardRequest;
-import com.sds.actlongs.domain.board.entity.Board;
-import com.sds.actlongs.domain.channel.entity.Channel;
-import com.sds.actlongs.domain.member.entity.Member;
-import com.sds.actlongs.domain.video.entity.Video;
-import com.sds.actlongs.vo.ImageExtension;
-import com.sds.actlongs.vo.VideoExtension;
+import com.sds.actlongs.controller.board.dto.BoardUpdateRequest;
+import com.sds.actlongs.controller.board.dto.BoardUpdateResponse;
 
 @Api(tags = "게시글 API")
 @RestController
@@ -33,18 +27,13 @@ public class BoardController {
 	@ApiOperation(value = "상세 조회 API", notes = "B001: 게시글 상세정보 조회에 성공하였습니다.")
 	@GetMapping
 	public ResponseEntity<BoardDetailResponse> getBoardDetail(@Valid @RequestBody final BoardRequest request) {
-		return ResponseEntity.ok(BoardDetailResponse.of(
-			new Video(
-				new Board(
-					new Member("harry", null, null),
-					new Channel("Knox SRE", new Member("din", null, null), null, null),
-					"재진스 플레이리스트",
-					"재진스의 뉴진스 플레이리스트 입니다."),
-				"static/스크린샷(11)_1686513849288",
-				ImageExtension.PNG,
-				"data/test_1686534272185",
-				VideoExtension.MP4,
-				Time.valueOf(LocalTime.now()))));
+		return ResponseEntity.ok(BoardDetailResponse.of(null));
+	}
+
+	@ApiOperation(value = "수정 API", notes = "B002: 게시글 수정에 성공하였습니다.")
+	@PatchMapping
+	public ResponseEntity<BoardUpdateResponse> updateBoard(@Valid @RequestBody final BoardUpdateRequest request) {
+		return ResponseEntity.ok(BoardUpdateResponse.of(null));
 	}
 
 }
