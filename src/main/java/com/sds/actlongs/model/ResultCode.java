6705e1344bcf952b1d0ab467589994ff35fadb23
eunsol <a2670624@naver.com>
2023-06-17 15:48:03 +0900

    [SL-158] fix: build error 스타일 재정렬

--- src/main/java/com/sds/actlongs/model/ResultCode.java
+++ src/main/java/com/sds/actlongs/model/ResultCode.java
@@ -8,20 +8,17 @@ import lombok.Getter;
 public enum ResultCode {
 
 	// Member
-	LOGIN_SUCCESS(200, "L001", "로그인에 성공하였습니다."),
-	LOGIN_FAILURE(200, "L002", "로그인에 실패하였습니다."),
-	MEMBERINFO_SUCCESS(200, "MI001", "회원정보 조회에 성공하였습니다."),
+	LOGIN_SUCCESS(200, "L001", "로그인에 성공하였습니다."), LOGIN_FAILURE(200, "L002", "로그인에 실패하였습니다."), MEMBERINFO_SUCCESS(200,
+		"MI001", "회원정보 조회에 성공하였습니다."),
 
 	// Channel
-	CHANNELCREATE_SUCCESS(200, "CC001", "그룹 생성에 성공하였습니다."),
-	CHANNELCREATE_FAILURE(200, "CC002", "그룹 생성에 실패하였습니다."),
-	CHANNELLIST_SUCCESS(200, "CL001", "그룹목록 조회에 성공하였습니다."),
-  
-  // Board
-	BOARDDETAIL_SUCCESS(200, "B001", "게시글 상세정보 조회에 성공하였습니다."),
-	BOARDUPDATE_SUCCESS(200, "B002", "게시글 수정에 성공하였습니다."),
-	BOARDDELETE_SUCCESS(200, "B003", "게시글 삭제에 성공하였습니다."),
-	BOARDLIST_SUCCESS(200, "B004", "게시글 검색에 성공하였습니다.");
+	CHANNELCREATE_SUCCESS(200, "CC001", "그룹 생성에 성공하였습니다."), CHANNELCREATE_FAILURE(200, "CC002",
+		"그룹 생성에 실패하였습니다."), CHANNELLIST_SUCCESS(200, "CL001", "그룹목록 조회에 성공하였습니다."),
+
+	// Board
+	BOARDDETAIL_SUCCESS(200, "B001", "게시글 상세정보 조회에 성공하였습니다."), BOARDUPDATE_SUCCESS(200, "B002",
+		"게시글 수정에 성공하였습니다."), BOARDDELETE_SUCCESS(200, "B003", "게시글 삭제에 성공하였습니다."), BOARDLIST_SUCCESS(200, "B004",
+		"게시글 검색에 성공하였습니다.");
 
 	private final int status;
 	private final String code;
