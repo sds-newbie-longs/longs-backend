b38de51c7b77f151b854d02883c50f74d2a87b7c
hongsam123 <a2670624@naver.com>
2023-06-20 08:52:05 +0900

    [SL-186] test: 게시글 목록 조회(메인화면) test code

--- src/test/java/com/sds/actlongs/service/board/BoardServiceImplTest.java
+++ src/test/java/com/sds/actlongs/service/board/BoardServiceImplTest.java
@@ -4,6 +4,8 @@ import static org.mockito.BDDMockito.*;
 
 import java.sql.Time;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -14,9 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sds.actlongs.controller.board.dto.BoardDto;
+import com.sds.actlongs.controller.board.dto.MemberBoardsDto;
 import com.sds.actlongs.domain.board.entity.Board;
 import com.sds.actlongs.domain.board.repository.BoardRepository;
 import com.sds.actlongs.domain.channel.entity.Channel;
+import com.sds.actlongs.domain.channelmember.entity.ChannelMember;
+import com.sds.actlongs.domain.channelmember.repository.ChannelMemberRepository;
 import com.sds.actlongs.domain.member.entity.Member;
 import com.sds.actlongs.domain.video.entity.Video;
 import com.sds.actlongs.domain.video.repository.VideoRepository;
@@ -31,6 +37,9 @@ class BoardServiceImplTest {
 
 	@Mock
 	private BoardRepository boardRepository;
+
+	@Mock
+	private ChannelMemberRepository channelMemberRepository;
 
 	@InjectMocks
 	private BoardServiceImpl subject;
@@ -136,6 +145,59 @@ class BoardServiceImplTest {
 
 		//then
 		Assertions.assertThat(result).isEqualTo(false);
+	}
+
+	@Test
+	@DisplayName("channelId로 메인페이지 전체 게시글 조회 요청을 하면 전체 게시글과 멤버별 게시글 리스트를 최신순으로 가져온다")
+	void ifGetMemberBoardsWithExistingChannelIdThenSuceess() {
+		//given
+		Member member1 = new Member("harry", null, null);
+		Member member2 = new Member("din", null, null);
+
+		Channel channel = new Channel("Knox SRE", member2, null, null);
+
+		Video video1 = new Video(
+			new Board(
+				member1,
+				channel,
+				"제목1",
+				"내용1"),
+			"썸네일uuid1",
+			ImageExtension.PNG,
+			"비디오uuid1",
+			VideoExtension.MP4,
+			Time.valueOf(LocalTime.now()));
+
+		Video video2 = new Video(
+			new Board(
+				member2,
+				channel,
+				"제목2",
+				"내용2"),
+			"썸네일uuid2",
+			ImageExtension.PNG,
+			"비디오uuid2",
+			VideoExtension.MP4,
+			Time.valueOf(LocalTime.now()));
+
+		ChannelMember channelMember1 = new ChannelMember(member1, channel);
+		ChannelMember channelMember2 = new ChannelMember(member2, channel);
+		given(channelMemberRepository.findByChannelId(channel.getId())).willReturn(
+			Arrays.asList(channelMember1, channelMember2));
+		given(videoRepository.findByBoardChannelIdOrderByCreatedAtDesc(channel.getId())).willReturn(
+			Arrays.asList(video2, video1));
+		given(videoRepository.findByBoardMemberIdOrderByCreatedAtDesc(member1.getId())).willReturn(
+			List.of(video1));
+		given(videoRepository.findByBoardMemberIdOrderByCreatedAtDesc(member2.getId())).willReturn(
+			List.of(video2));
+
+		//when
+		List<BoardDto> boardList = subject.getBoardList(channel.getId());
+		List<MemberBoardsDto> memberBoardsList = subject.getMemberBoardsList(channel.getId());
+
+		//then
+		Assertions.assertThat(boardList.size()).isEqualTo(2);
+		Assertions.assertThat(memberBoardsList.size()).isEqualTo(2);
 	}
 
 }
