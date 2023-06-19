1e4ac16035995e5c07e01d35b2298aa32e544aed
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2023-06-19 16:57:28 +0900

    [SL-174] Ops:Spring Actuator를 올리기 위한 작업 (#39)
    * [SL-174] Ops:Spring Actuator를 올리기 위한 작업
    
    * [NO-ISSUE] cicd : formatter 적용
    
    * [NO-ISSUE] cicd : formatter 적용2(포매터 바보야)
    
    * [NO-ISSUE] cicd : formatter 적용2(포매터 바보야)
    
    * [NO-ISSUE] cicd : formatter 적용4(포매터 바보야)
    
    ---------
    
    Co-authored-by: vidigummy <vidigummy@gamil.com>

--- src/main/java/com/sds/actlongs/config/SwaggerConfig.java
+++ src/main/java/com/sds/actlongs/config/SwaggerConfig.java
@@ -1,10 +1,18 @@
 package com.sds.actlongs.config;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,6 +20,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Profile({"local", "dev"})
@@ -20,9 +30,41 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	@Bean
+	public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(
+				List<T> mappings) {
+				List<T> copy = mappings.stream()
+					.filter(mapping -> mapping.getPatternParser() == null)
+					.collect(Collectors.toList());
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>)field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
+	}
+
+	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.useDefaultResponseMessages(false)
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
 			.apiInfo(apiInfo())
 			.select()
 			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
@@ -32,11 +74,7 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-			.title("Longs's API Docs")
-			.version("1.0")
-			.description("API 명세서")
-			.build();
+		return new ApiInfoBuilder().title("Longs's API Docs").version("1.0").description("API 명세서").build();
 	}
 
 }
