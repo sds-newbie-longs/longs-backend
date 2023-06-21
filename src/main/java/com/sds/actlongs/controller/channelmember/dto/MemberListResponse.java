fdf7eb50e8793474fc1b4ecf7bca3ef836bce62f
Sean_R <ksp970306@gmail.com>
2023-06-21 20:21:38 +0900

    [NO-ISSUE] feat: 그룹 회원 목록 조회 API 응답 모델 수정

--- src/main/java/com/sds/actlongs/controller/channelmember/dto/MemberListResponse.java
+++ src/main/java/com/sds/actlongs/controller/channelmember/dto/MemberListResponse.java
@@ -1,7 +1,7 @@
 package com.sds.actlongs.controller.channelmember.dto;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -13,12 +13,20 @@ import com.sds.actlongs.model.ResultResponse;
 @Getter
 public class MemberListResponse extends ResultResponse {
 
-	@ApiModelProperty(position = 4, value = "그룹원 목록", example = ""
-		+ "["
-		+ "{ username: Din },"
-		+ "{ username: Ari },"
-		+ "{ username: Kang }"
-		+ "]")
+	@ApiModelProperty(position = 4, value = "그룹원 목록", example = "[\n"
+		+ "    {\n"
+		+ "      \"id\": 1,\n"
+		+ "      \"username\": \"Din\"\n"
+		+ "    },\n"
+		+ "    {\n"
+		+ "      \"id\": 2,\n"
+		+ "      \"username\": \"Ari\"\n"
+		+ "    },\n"
+		+ "    {\n"
+		+ "      \"id\": 3,\n"
+		+ "      \"username\": \"Kang\"\n"
+		+ "    }\n"
+		+ "  ]")
 	private List<MemberResponse> memberList;
 
 	public MemberListResponse(List<MemberResponse> memberList) {
@@ -26,12 +34,11 @@ public class MemberListResponse extends ResultResponse {
 		this.memberList = memberList;
 	}
 
-	public static MemberListResponse from(List<ChannelMember> members) {
-		List<MemberListResponse.MemberResponse> memberList = new ArrayList<>();
-		for (ChannelMember cm : members) {
-			memberList.add(MemberResponse.from(cm.getMember().getUsername()));
-		}
-		return new MemberListResponse(memberList);
+	public static MemberListResponse from(List<ChannelMember> channelMembers) {
+		return new MemberListResponse(channelMembers.stream()
+			.map(ChannelMember::getMember)
+			.map(member -> MemberResponse.from(member.getId(), member.getUsername()))
+			.collect(Collectors.toList()));
 	}
 
 	@Getter
@@ -39,13 +46,16 @@ public class MemberListResponse extends ResultResponse {
 
 		@ApiModelProperty(value = "아이디", example = "Harry")
 		private String username;
+		@ApiModelProperty(value = "PK", example = "1")
+		private Long id;
 
-		public MemberResponse(String username) {
+		public MemberResponse(Long id, String username) {
+			this.id = id;
 			this.username = username;
 		}
 
-		public static MemberResponse from(String username) {
-			return new MemberResponse(username);
+		public static MemberResponse from(Long id, String username) {
+			return new MemberResponse(id, username);
 		}
 
 	}
