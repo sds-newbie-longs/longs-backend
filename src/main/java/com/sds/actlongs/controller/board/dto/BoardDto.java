062ba42becef167279a3d7c51a019d28ca7ab169
hongsam123 <a2670624@naver.com>
2023-06-26 13:41:39 +0900

    [SL-234] feat: 게시글 목록 조회시 status 가져오기 & deleted만 빼고 조회로 변경

--- src/main/java/com/sds/actlongs/controller/board/dto/BoardDto.java
+++ src/main/java/com/sds/actlongs/controller/board/dto/BoardDto.java
@@ -5,6 +5,7 @@ import static com.sds.actlongs.util.Constants.*;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
+import com.sds.actlongs.domain.board.entity.Board;
 import com.sds.actlongs.domain.video.entity.Video;
 
 @Getter
@@ -20,6 +21,9 @@ public class BoardDto {
 	private final String description;
 	@ApiModelProperty(value = "썸네일 url", example = "https://act-longs.s3.ap-northeast-2.amazonaws.com/thumbnails/c0c5afcaaad24d91bfb777440ef3bc12.png")
 	private final String thumbnailUrl;
+	@ApiModelProperty(value = "상태값", example = "COMPLETED")
+	private final Board.Status status;
+
 
 	public BoardDto(Video video) {
 		this.boardId = video.getBoard().getId();
@@ -28,6 +32,7 @@ public class BoardDto {
 		this.description = video.getBoard().getDescription();
 		this.thumbnailUrl = S3_BUCKET_HOST_THUMBNAIL_URL + CATEGORY_PREFIX + video.getThumbnailImageUuid() + DOT
 			+ video.getThumbnailImageType().name().toLowerCase();
+		this.status = video.getBoard().getStatus();
 	}
 
 }
--- src/main/java/com/sds/actlongs/domain/video/repository/VideoRepository.java
+++ src/main/java/com/sds/actlongs/domain/video/repository/VideoRepository.java
@@ -12,25 +12,25 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
 
 	@Query("SELECT v "
 		+ "FROM videos v "
-		+ "INNER JOIN v.board b ON b.status = 'CREATED' "
+		+ "INNER JOIN v.board b ON b.status <> 'DELETED' "
 		+ "WHERE v.board.id = :boardId")
 	Optional<Video> findByBoardId(Long boardId);
 
 	@Query("SELECT v "
 		+ "FROM videos v "
-		+ "INNER JOIN v.board b ON b.channel.id = :channelId AND b.status = 'CREATED' "
+		+ "INNER JOIN v.board b ON b.channel.id = :channelId AND b.status <> 'DELETED' "
 		+ "ORDER BY v.createdAt DESC")
 	List<Video> findByBoardChannelIdOrderByCreatedAtDesc(Long channelId);
 
 	@Query("SELECT v "
 		+ "FROM videos v "
-		+ "INNER JOIN v.board b ON b.member.id = :memberId AND b.status = 'CREATED' "
+		+ "INNER JOIN v.board b ON b.member.id = :memberId AND b.status <> 'DELETED' "
 		+ "ORDER BY v.createdAt DESC")
 	List<Video> findByBoardMemberIdOrderByCreatedAtDesc(Long memberId);
 
 	@Query("SELECT v "
 		+ "FROM videos v "
-		+ "INNER JOIN boards b ON v.board.id = b.id AND b.status = 'CREATED' "
+		+ "INNER JOIN boards b ON v.board.id = b.id AND b.status <> 'DELETED' "
 		+ "WHERE b.channel.id = :channelId AND b.title LIKE %:keyword% "
 		+ "ORDER BY v.createdAt DESC")
 	List<Video> findAllByChannelIdAndKeywordContainingOrderByCreatedAtDesc(Long channelId, String keyword);
