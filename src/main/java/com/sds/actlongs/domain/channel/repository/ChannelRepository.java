6689fe9c681bf1700a8d5b852625e5945b3f8cbe
nureesong <nuri3136@naver.com>
2023-06-28 16:18:44 +0900

    [NO-ISSUE] fix: 그룹생성 시 그룹명 중복체크 쿼리문 수정

--- src/main/java/com/sds/actlongs/domain/channel/repository/ChannelRepository.java
+++ src/main/java/com/sds/actlongs/domain/channel/repository/ChannelRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sds.actlongs.domain.channel.entity.Channel;
@@ -14,6 +15,7 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
 	List<Channel> findByIdIn(Collection<Long> ids);
 
+	@Query("SELECT c FROM channels c WHERE c.status = 'CREATED' AND c.channelName = :channelName")
 	Optional<Channel> findByChannelName(final String channelName);
 
 	Optional<Channel> findByIdAndOwnerId(Long channelId, Long memberId);
